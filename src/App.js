45bb12597e5e424a2bbf770aa8192df8efb3e5e9
Ayomide Jones <ayormeday@gmail.com>
2019-09-19 23:22:52 +0100

    included episode list and card components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 
 
+const api = "https://rickandmortyapi.com/documentation";
+const characterApi ="https://rickandmortyapi.com/api/character/";
+const locationApi = "https://rickandmortyapi.com/api/location/";
+const episodeApi = "https://rickandmortyapi.com/api/episode/";
+
 export default function App() {
   return (
     <main>
