ce9180d8182e6d56a7b5b6787ed6a3fa10e21117
Ayomide Jones <ayormeday@gmail.com>
2019-09-20 15:50:32 +0100

    working on search functionality

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,34 +2,44 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard";
 
+const characterApi = "https://rickandmortyapi.com/api/character/";
 
-const characterApi = "https://rickandmortyapi.com/api/character/"
+
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const [character, setCharacter] = useState([]);
-  const [serverError, setServerError] = useState('');
+  const [characters, setCharacters] = useState([]);
+  const [display, setDisplay] = useState([]);
+  const [searchString, setSearchString] = useState("");
+
+  // (const [charactertodisplay])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    axios.get(characterApi)
-    .then(response => {
-      setCharacter(response.data.results);
-    })
+    axios
+      .get(characterApi)
+      .then(response => {
+        setCharacters(response.data.results);
+      })
       .catch(error => {
-        setServerError(error.message);
-    })
+        console.log(error.message);
+      });
   }, []);
+
+  useEffect(() => {
+    setDisplay(characters);
+  }, [characters]); 
+
 
   return (
     <section className="character-list grid-view">
-      <h2>{
-        character.map(character => {
-          return (
-            <CharacterCard key={character.id} character={character}/>
-          )
-        })
-      }</h2>
+      <h2>
+        {display.map(character => {
+          return <CharacterCard key={character.id} character={character} />;
+        })}
+      </h2>
     </section>
   );
 }
+
+
